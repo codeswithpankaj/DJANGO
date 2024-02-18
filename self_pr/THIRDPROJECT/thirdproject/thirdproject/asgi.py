aca5b5d85b16eaaf64d90f8523b6412a2ea779d4
AnilKumar Prajapati <150881412+Ak-andy@users.noreply.github.com>
2024-02-18 13:52:24 +0000

    members

--- self_pr/THIRDPROJECT/thirdproject/thirdproject/asgi.py
+++ self_pr/THIRDPROJECT/thirdproject/thirdproject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for THIRDPROJECT project.
+ASGI config for thirdproject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "THIRDPROJECT.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thirdproject.settings")
 
 application = get_asgi_application()
--- self_pr/THIRDPROJECT/thirdproject/thirdproject/wsgi.py
+++ self_pr/THIRDPROJECT/thirdproject/thirdproject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for THIRDPROJECT project.
+WSGI config for thirdproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "THIRDPROJECT.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thirdproject.settings")
 
 application = get_wsgi_application()
