250c2460de88199456200a41c2bedc9f7c90b687
AnilKumar Prajapati <150881412+Ak-andy@users.noreply.github.com>
2024-02-25 12:31:30 +0000

    STATIC

--- CLass/3_pr_static/static_files/Home/views.py
+++ CLass/3_pr_static/static_files/Home/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.templatetags.static import static
 
 def first(request):
+   css_url = static('css/bootstrap.min.css')
+   # Do something with the CSS URL
+
+def firstpage(request):
+    return render(request,"index.html")
+
+'''
+def first(request):
+
    return HttpResponse("hello andy")
+   '''
