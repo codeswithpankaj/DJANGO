1da7bb6c0e3663105b217c2debc2aa777c9f8598
AnilKumar Prajapati <150881412+Ak-andy@users.noreply.github.com>
2024-02-25 02:39:51 +0000

    dnamic

--- self_pr/THIRDPROJECT/Template_Engine/table/views.py
+++ self_pr/THIRDPROJECT/Template_Engine/table/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def my_table(request):
-    return render(request,"table.html")
+
+    #sending dynamic data to the html file
+    peoples=[{'name':'Anil','age':24},
+             {'name':'Omkar','age':13},
+             {'name':'Salim','age':22}]
+    #'context' is used to send backend data to user 
+    context={'user':peoples}
+    return render(request,"table.html",context)
